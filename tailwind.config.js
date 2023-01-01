617d2eff11b67f4b07093439bf94acec5f9ce56d
firdloushdw <firdloushdw@upi.edu>
2023-01-01 07:11:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'solution': "url('src/bgbg.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
